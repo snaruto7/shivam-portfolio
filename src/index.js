b88e9884464f483276e92226dc816ae4e57d287e
Shivam Narula <shivam.narula7@gmail.com>
2022-06-10 07:09:15 +0530

    Added 1st analytics event

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import analytics from "firebase/analytics"
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
+await analytics().logEvent('App', {
+    id: 1,
+    status: started,
+})
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
