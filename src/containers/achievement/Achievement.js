b6615f265241f78dd8c9326928f25b9f347442aa
Shivam Narula <shivam.narula7@gmail.com>
2022-10-15 19:53:44 +0530

    enabled 1 cert

--- src/containers/achievement/Achievement.js
+++ src/containers/achievement/Achievement.js
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import "./Achievement.scss";
-//import AchievementCard from "../../components/achievementCard/AchievementCard";
+import AchievementCard from "../../components/achievementCard/AchievementCard";
 import {achievementSection} from "../../portfolio";
 import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
@@ -33,7 +33,7 @@ export default function Achievement() {
               {achievementSection.subtitle}
             </p>
           </div>
-          {/* <div className="achievement-cards-div">
+          <div className="achievement-cards-div">
             {achievementSection.achievementsCards.map((card, i) => {
               return (
                 <AchievementCard
@@ -48,7 +48,7 @@ export default function Achievement() {
                 />
               );
             })}
-          </div> */}
+          </div>
         </div>
       </div>
     </Fade>
